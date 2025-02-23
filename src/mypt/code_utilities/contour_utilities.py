46a26c5a57d188b46982f279b19da8d6e404fd37
ayhem18 <bouabidayhem@gmail.com>
2025-02-23 15:27:45 +0300

    improving the contour_utilities

--- src/mypt/code_utilities/contour_utilities.py
+++ src/mypt/code_utilities/contour_utilities.py
@@ -15,12 +15,30 @@ NUM = Union[float, int]
 def interpolate_between_two_points(p1: Tuple[int, int], p2: Tuple[int, int], num_points: int) -> CONTOUR:
     y1, x1 = p1
     y2, x2 = p2
-    slope1 = (y2 - y1 )  / (num_points + 1)
+    slope1 = (y2 - y1)  / (num_points + 1)
     slope2 = (x2 - x1)  / (num_points + 1)
     res =  [(int(y1 + slope1 * (i + 1)), int(x1 + slope2 * (i + 1))) for i in range(num_points)]
     return res
 
-def stretch_contour(contour: CONTOUR, coefficient: NUM, y_x: bool = True, x_axis=True):
+
+def stretch_contour(contour: CONTOUR, coefficient: NUM, y_x: bool = True, x_axis: bool = True):
+    if x_axis:
+        return stretch_contour_x_axis(contour, coefficient, y_x)
+    else:
+        return stretch_contour_y_axis(contour, coefficient, y_x)
+
+
+def stretch_contour_y_axis(contour: CONTOUR, coefficient: NUM, y_x: bool = True):
+    # the idea is as follows:
+    # 1. rotate the contour over the y = x line
+    # 2. stretch the contour with the stretch_contour_x_axis function
+    # 3. rotate the contour back
+    rotated_contour = [(c2, c1) for c1, c2 in contour]
+    stretched_contour = stretch_contour_x_axis(rotated_contour, coefficient, y_x)
+    return [(c2, c1) for c1, c2 in stretched_contour]
+
+
+def stretch_contour_x_axis(contour: CONTOUR, coefficient: NUM, y_x: bool = True):
 
     if coefficient <= 1:
         raise ValueError(f"The coefficient argument must be larger than 1. Found: {coefficient}")
@@ -39,7 +57,17 @@ def stretch_contour(contour: CONTOUR, coefficient: NUM, y_x: bool = True, x_axis
     mpx = middle_point[1]
 
     new_contour = [(y, int(mpx + (x - mpx) * coefficient)) for y, x in sorted_contour]
-    
+
+    # if the minimum value in the new_contour is less than 0, then we need to shift the contour to the right: to avoid negative values
+    # compute the minimum value in the new_contour
+    min_x_new_coordinate = min(new_contour, key=lambda p: p[1])[1]
+
+    # if the minimum value is less than 0, then we need to shift the contour to the right
+    if min_x_new_coordinate < 0:
+        new_contour = [(y, x + abs(min_x_new_coordinate)) for y, x in new_contour]
+
+    assert all([x >= 0 for _, x in new_contour]), f"The new contour contains negative values. Found: {new_contour}"
+
     # iterate and interpolate
     num_points = int(max(1, coefficient // 2))  
     interpolation_points = [interpolate_between_two_points(new_contour[i], new_contour[i + 1], num_points) for i in range(len(new_contour) - 1)]
